33710e522fab5dd5d46218fdc70ec383d8ddd323
Javier Muñoz Tous <javimtib92@gmail.com>
2024-09-27 20:03:06 +0200

    feat: implement home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <div>Hello world!</div>;
+import Home from "@/src/home/presentation/pages/home-page/home-page";
+
+export default function HomePage() {
+  return <Home />;
 }
