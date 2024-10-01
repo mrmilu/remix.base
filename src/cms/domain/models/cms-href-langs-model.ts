dc80ed55f09ab6b622a59db6e27645aedbd59bc6
Javier Muñoz Tous <javimtib92@gmail.com>
2024-10-01 13:50:11 +0200

    fix: match locales with expected locales from json api

--- src/cms/domain/models/cms-href-langs-model.ts
+++ src/cms/domain/models/cms-href-langs-model.ts
@@ -3,6 +3,6 @@ import type { Languages } from "@/src/shared/presentation/i18n";
 export type CmsHrefLangsModel = Record<Languages, string | null>;
 
 export const createEmptyCmsHrefLangsModel = (): CmsHrefLangsModel => ({
-  en: null,
-  es: null
+  "en-GB": null,
+  "es-ES": null
 });
--- src/shared/presentation/i18n/index.ts
+++ src/shared/presentation/i18n/index.ts
@@ -1,22 +1,22 @@
 import { serverOnly$ } from "vite-env-only/macros";
 
-import enTranslation from "@/src/shared/presentation/i18n/locales/en";
-import esTranslation from "@/src/shared/presentation/i18n/locales/es";
+import enTranslation from "@/src/shared/presentation/i18n/locales/es-ES";
+import esTranslation from "@/src/shared/presentation/i18n/locales/en-GB";
 
 // This is the list of languages your application supports, the last one is your
 // fallback language
-export const supportedLngs = ["es", "en"] as const;
+export const supportedLngs = ["es-ES", "en-GB"] as const;
 
 // This is the language you want to use in case
 // the user language is not in the supportedLngs
-export const fallbackLng = "es";
+export const fallbackLng = "es-ES";
 
 // The default namespace of i18next is "translation", but you can customize it
 export const defaultNS = "translation";
 
 const translationResources = {
-  en: { translation: enTranslation },
-  es: { translation: esTranslation }
+  "es-ES": { translation: enTranslation },
+  "en-GB": { translation: esTranslation }
 } as const;
 
 export type resources = typeof translationResources;
